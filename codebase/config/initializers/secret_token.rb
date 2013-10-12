f3a67f3c55feb57412aab759c1b17d527a31c85b
Michaelangelo <michaelghinrichs@gmail.com>
2013-10-12 15:14:55 -0700

    initial commit

---
+++ codebase/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Codebase::Application.config.secret_token = 'a464f81d4cb56004e70e8dc65b29eeca8a8bb69b743b15b1273e87eb1a2ce53ed7aca728fa3ef5f471f92b4705e8d666872f7248c0515b47df1487202d955eed'
